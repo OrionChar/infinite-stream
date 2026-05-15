594e11d3c2943a76b0ce98f6f0b630e16ac4ca97
Your Name <you@example.com>
2026-05-15 08:44:17 +0300

    Decorate logs

--- src/pipeline/pipeline-logger.ts
+++ src/pipeline/pipeline-logger.ts
@@ -1,17 +1,22 @@
 import PipelineStage from "./pipiline-stage.js";
+import chalk, { ChalkInstance } from "chalk";
 
 export default class PipelineLogger {
     log(stage: PipelineStage<unknown>) {
         stage.on('task:start', (stageName, payload) => {
-            console.log('START', stageName, payload.location)
+            console.log(this.infoStyle('START'), stageName, payload.location)
         })
 
         stage.on('task:complete', (stageName, payload) => {
-            console.log('COMPLETE', stageName, payload.location)
+            console.log(this.successStyle('COMPLETE'), stageName, payload.location)
         })
 
         stage.on('task:error', (error) => {
-            console.error('ERROR', error)
+            console.log(this.errorStyle('ERROR'), error.toString())
         })
     }
+
+    private errorStyle: ChalkInstance = chalk.bgRed.white
+    private infoStyle: ChalkInstance = chalk.bgBlue.white
+    private successStyle: ChalkInstance = chalk.bgGreen.white
 }
